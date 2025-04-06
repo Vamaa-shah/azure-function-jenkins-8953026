8adad3fa42bf7e0936c6a733551e0ef724ac65c0
Vamaa-shah <vamashah.1123@gmail.com>
2025-04-05 21:10:10 -0400

    Final test file with working HelloWorld function

--- src/functions/HelloWorld.js
+++ src/functions/HelloWorld.js
@@ -1,13 +1,7 @@
-const { app } = require('@azure/functions');
-
-app.http('HelloWorld', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
-
-        const name = request.query.get('name') || await request.text() || 'world';
-
-        return { body: `Hello, ${name}!` };
-    }
-});
+module.exports = async function (context, req) {
+    context.res = {
+      status: 200,
+      body: "Hello, World!"
+    };
+  };
+  
--- test/hello.test.js
+++ test/hello.test.js
@@ -1,33 +1,25 @@
-// test/hello.test.js
+const response = require("../src/functions/HelloWorld.js");
 
 describe("Hello World Function", () => {
-    test("should return 'Hello, World!'", () => {
-      const context = {};
-      const req = {};
-      const response = require("../index");
-  
-      return response(context, req).then(() => {
-        expect(context.res.status).toBe(200);
-        expect(context.res.body).toBe("Hello, World!");
-      });
-    });
-  
-    test("should return 200 status code", async () => {
-      const context = {};
-      const req = {};
-      const response = require("../index");
-  
-      await response(context, req);
-      expect(context.res.status).toBe(200);
-    });
-  
-    test("should return a non-empty response", async () => {
-      const context = {};
-      const req = {};
-      const response = require("../index");
-  
-      await response(context, req);
-      expect(context.res.body).toBeTruthy();
-    });
+  test("should return 'Hello, World!'", async () => {
+    const context = {};
+    const req = {};
+    await response(context, req);
+    expect(context.res.status).toBe(200);
+    expect(context.res.body).toBe("Hello, World!");
   });
-  
+
+  test("should return 200 status code", async () => {
+    const context = {};
+    const req = {};
+    await response(context, req);
+    expect(context.res.status).toBe(200);
+  });
+
+  test("should return a non-empty response", async () => {
+    const context = {};
+    const req = {};
+    await response(context, req);
+    expect(context.res.body).toBeTruthy();
+  });
+});
